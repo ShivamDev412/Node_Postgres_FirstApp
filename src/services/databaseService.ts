02b220218a5dc4c2306b35456c4a630e7dc1f49c
Shivam Singh <shiv41297@gmail.com>
2024-01-02 07:49:57 -0500

    update code for database connection

--- src/services/databaseService.ts
+++ src/services/databaseService.ts
@@ -3,10 +3,12 @@ import dotenv from "dotenv";
 dotenv.config();
 const { Pool } = pg;
 const connection = new Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  port: 5432,
-  host: "localhost",
-  database: "perntodo",
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASSWORD,
+  // port: 5432,
+  // host: "localhost",
+  // database: "perntodo",
 });
-export {connection as dbConnection};
+export { connection as dbConnection };
